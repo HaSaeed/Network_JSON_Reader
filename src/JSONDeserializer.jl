5ba122471e366e97a97b14a0b024f8bd6cd52550
Hassan Saeed <hassan.saeed@tum.de>
2023-09-16 18:27:54 +0200

    Demands Update

--- src/JSONDeserializer.jl
+++ src/JSONDeserializer.jl
@@ -13,24 +13,31 @@ function readgraph(filepath::String)
     gr_links = DataFrame(jsontable(json3df[1,:links]))
     gr_n = select(gr_nodes, [:stop_name, :node_id, :x, :y])
     gr_l = select(gr_links, [:node_id_1, :node_id_2, :distance, :traffic])
+    gr_d = DataFrame(jsontable(json3df[1,:demands]))
 
     gr = MetaDiGraph()
 
     for (i, row) in enumerate( eachrow( gr_n ) )
         @assert add_vertex!(gr)
-        set_prop!(gr, i, :name, row["node_id"])
-        set_prop!(gr, i, :id, row["stop_name"])
+        set_prop!(gr, i, :name, row["stop_name"])
+        set_prop!(gr, i, :id, row["node_id"])
         set_prop!(gr, i, :xcoord, row["x"])
         set_prop!(gr, i, :ycoord, row["y"])
     end
     set_indexing_prop!(gr, :name)
 
     for (i, row) in enumerate( eachrow( gr_l ) )
-        @assert add_edge!(gr, gr[row["node_id_1"], :name], gr[row["node_id_2"], :name])
-        @assert add_edge!(gr, gr[row["node_id_2"], :name], gr[row["node_id_1"], :name])
-        set_prop!(gr, gr[row["node_id_1"], :name], gr[row["node_id_2"], :name], :length, row["distance"])
-        set_prop!(gr, gr[row["node_id_2"], :name], gr[row["node_id_1"], :name], :length, row["distance"])
-        set_prop!(gr, gr[row["node_id_1"], :name], gr[row["node_id_2"], :name], :demand, row["traffic"])
+        @assert add_edge!(gr, gr[row["node_name_1"], :name], gr[row["node_name_2"], :name])
+        @assert add_edge!(gr, gr[row["node_name_2"], :name], gr[row["node_name_1"], :name])
+        set_prop!(gr, gr[row["node_name_1"], :name], gr[row["node_name_2"], :name], :length, row["distance"])
+        set_prop!(gr, gr[row["node_name_2"], :name], gr[row["node_name_1"], :name], :length, row["distance"])
+    end
+
+    weightfield!(gr, :length)
+
+    for (i, row) in enumerate( eachrow( gr_d ) )
+        demandvalue = parse(Float64, row["demandValue"])
+        set_prop!(gr, gr[row["source"], :name], gr[row["target"], :name], :demand, demandvalue)    
     end
 
     return gr
@@ -45,15 +52,14 @@ function readdemands(filepath::String; scale=1)
     json3str = JSON3.read(dicttxt, jsonlines=true)
     json3table = jsontable(json3str)
     json3df = DataFrame(json3table)
-    gr_links = DataFrame(jsontable(json3df[1,:links]))
-    demands = select(gr_links, [:node_id_1, :node_id_2, :traffic])
+    gr_demands = DataFrame(jsontable(json3df[1,:demands]))
 
     ds = Dict{Tuple{String, String}, Float64}()
-    for (i, row) in enumerate( eachrow( demands ) )
-        demandsource = row["node_id_1"]
-        demandtarget = row["node_id_2"]
-        demandvalue = row["traffic"]
-        
+    for (i, row) in enumerate( eachrow( gr_demands ) )
+        demandvalue = parse(Float64, row["demandValue"])
+        demandsource = row["source"]
+        demandtarget = row["target"]
+        demandvalue = demandvalue
         ds[(demandsource, demandtarget)] = demandvalue * scale
     end
 
